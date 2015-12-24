a59103d9967de9f96b8ecb63712ca33929b8ba48
Gabriel Florit <gabrielflorit@gmail.com>
2015-12-24 18:31:35 -0500

    Finish splitResults and bin/APtoJSON

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
+var fs = require('fs');
+var chalk = require('chalk');
+
 module.exports = function splitResults(json, directory) {
-	console.log(json.nextrequest);
+
+	// save each json.races item as a separate file
+	json.races.forEach(function(race, index, array) {
+
+		// create file name
+		var state = race.reportingUnits[0].statePostal;
+		var party = race.party;
+		var raceType = race.raceType;
+		var filename = directory + '/' + [state, party, raceType].join('-') + '.json';
+
+		fs.writeFileSync(filename, JSON.stringify(race, null, 2));
+		console.log(chalk.green('Writing ' + filename));
+
+	});
+
 }
 
