167b023cc8a12095455ff1f12fa8863eb778b48b
Gabriel Florit <gabrielflorit@gmail.com>
2016-01-04 15:13:28 -0500

    add election date to race

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var chalk = require('chalk');
 
 module.exports = function splitResults(json, directory) {
 
+	var electionDate = json.electionDate;
+
 	// save each json.races item as a separate file
 	json.races.forEach(function(race, index, array) {
 
@@ -11,6 +13,9 @@ module.exports = function splitResults(json, directory) {
 		var party = race.party;
 		var raceType = race.raceType;
 		var filename = directory + '/' + [state, party, raceType].join('-') + '.json';
+
+		// add election date to race
+		race.electionDate = electionDate;
 
 		fs.writeFileSync(filename, JSON.stringify(race, null, 2));
 		console.log(chalk.green('Writing ' + filename));
